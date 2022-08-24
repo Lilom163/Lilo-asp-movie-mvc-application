864cd710a5e8b62e9a9ad5f089214d16e6dc2340
Lilo Moykwueni <Lilo.Moykweni@Mediclinic.co.za>
2022-08-24 14:24:42 +0200

    Add project files.

---
+++ MediMoovi/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MediMoovi.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
